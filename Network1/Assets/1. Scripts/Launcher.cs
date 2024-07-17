b8069d0c8d839c47b4208b9a6e9f38fa1cd3e146
h6011_House <a01048648770@gmail.com>
2024-07-18 04:37:34 +0900

    Font + UI + 초반 작업

--- Network1/Assets/1. Scripts/Launcher.cs
+++ Network1/Assets/1. Scripts/Launcher.cs
@@ -5,8 +5,29 @@ using Photon.Pun;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+
+    LobbyCanvasManager lobbyCanvasManager;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void Start()
     {
+        lobbyCanvasManager = LobbyCanvasManager.Instance;
+
+
+        PhotonNetwork.NickName = $"Player {Random.Range(1000, 9999).ToString()}";
+
         Debug.Log("Connecting to Master");
         PhotonNetwork.ConnectUsingSettings();
     }
@@ -19,9 +40,44 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public override void OnJoinedLobby()
     {
+        lobbyCanvasManager.VisibleOnly("Main");
         Debug.Log("Joined Lobby");
     }
 
+    public void CreateRoom(string _roomName)
+    {
+        if (string.IsNullOrEmpty(_roomName))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(_roomName);
+        lobbyCanvasManager.VisibleOnly("Loading");
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        lobbyCanvasManager.VisibleOnly("CurrentRoom");
+        lobbyCanvasManager.currentRoomNameText.text = PhotonNetwork.CurrentRoom.Name;
+        //lobbyCanvasManager.setCurrentRoomNameText(PhotonNetwork.CurrentLobby.Name);
+    }
+
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        lobbyCanvasManager.visibleError(message);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        lobbyCanvasManager.VisibleOnly("Loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        lobbyCanvasManager.VisibleOnly("Menu");
+    }
 
 
 }
