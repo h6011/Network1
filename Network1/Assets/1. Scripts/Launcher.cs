ca8c7a4a7edea45eee5b4078327271f0051be7ff
h6011_House <a01048648770@gmail.com>
2024-07-18 03:29:29 +0900

    유니티 버전 바꿈, 다시 재설치 제기랄

---
+++ Network1/Assets/1. Scripts/Launcher.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        Debug.Log("Connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+
+
+
+}
